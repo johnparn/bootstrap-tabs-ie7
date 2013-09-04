0ac2fa5be765fed46cd04b0ccfb094b5a7fa67c2
johnparn <john.parnefjord@gmail.com>
2013-09-04 17:19:14 +0200

    Did some house cleaning

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var App = angular.module('App', ['ui.bootstrap.tpls', 'ui.bootstrap.tabs']);
+var App = angular.module('App', ['ui.bootstrap.tabs']);
 
 App.controller('MainCtrl', function($scope, $routeParams) {
 
